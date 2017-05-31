add3f37ea58703f57abfee057125f44646276e88
Andrey Polischuk <me@andrepolischuk.com>
2017-05-31 18:07:25 +0300

    Add temlate images support for tray icons

--- utils/trayIcon.js
+++ utils/trayIcon.js
@@ -4,5 +4,5 @@ const {join} = require('path')
 module.exports = function trayIcon (stage = '') {
   const iconName = stage === 'finish' ? '' : stage.replace(/^\w/, m => m.toUpperCase())
 
-  return join(__dirname, '..', `assets/tray${iconName}Icon.png`)
+  return join(__dirname, '..', `assets/tray${iconName}Template.png`)
 }
